67baf366bbde0dc5767e9513eef33c729b091f83
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2022-05-17 13:04:28 +0100

    Created utility.h file for the bank management system

---
+++ include/utility.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace hayohtee
+{
+    void clear_screen();
+    void show_header();
+    void show_main_menu();
+    void show_account_form();
+    void show_transaction_form();
+    void show_balance_form();
+    void show_account_holder_form();
+    void show_account_delete_form();
+    void show_account_update_form();
+    void sleep(int duration);
+}
